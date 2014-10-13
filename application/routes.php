206fcd10a5b886c5dff3676ba5cf5abdeaa7e1f5
Livingstone Fultang <livingstonefultang@gmail.com>
2014-10-13 20:56:32 +0100

    Updating app routes

--- application/routes.php
+++ application/routes.php
@@ -24,30 +24,3 @@ $app->route->attachResource("/project", "Project"); //multiple projects types an
 $app->route->add("/protocols{format}", "protocols", "Protocol");
 $app->route->attachResource("/protocol", "Protocol"); //a collection of tasks
 
-
-
-//$app->route->add("/callme", "callme")
-//	->setValues(array(
-//		"action"=>function($response, $params = null)use($app){
-//
-//			//var_dump($params);
-//
-//			$response->setContentType("json");
-//
-//			return $response->addContent("{j:s,o:n}");
-//
-//		})
-//	);
-//
-////Grouping routes under a prefix;
-//Route::attach("/blog", "blog", function($route){
-//	$route->setTokens(array(
-//	   'id'		=> '\d+',
-//	   'format'	=>'(\.json|\.atom|\.html)'
-//	));
-//   //subroutes
-//   $route->add( '{format}','browse');
-//   $route->add('/{id}{format}', "read");
-//   $route->add('/{id}/edit{format}', "edit");
-//
-//});
