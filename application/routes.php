5536323032e12d3775b6fa1968a16761a6d4c9ae
Livingstone Fultang <livingstonefultang@gmail.com>
2014-10-16 20:58:00 +0100

    Moving the project and Task controllers to livingstonef/budkit-pm (a separate package)

--- application/routes.php
+++ application/routes.php
@@ -7,7 +7,7 @@ $app->route->add("/", "Home");
 $app->route->attachResource("/inbox", "Inbox"); //a collection of streams;
 $app->route->attachResource("/message", "Message"); //controller should extend post;
 $app->route->attachResource("/notification", "Notification");
-$app->route->attachResource("/task", "Task"); //tasks? tasks are a collection of protocols mark task as results?
+
 $app->route->attachResource("/note", "Note"); //notes?
 $app->route->attachResource("/event", "Event"); //multiple event types and status, e.g proposed meting
 $app->route->attachResource("/stream", "Stream"); //collection of resources,
@@ -15,9 +15,6 @@ $app->route->attachResource("/person", "Person"); //persons have different roles
 $app->route->attachResource("/group", "Group"); //collection of persons?
 $app->route->attachResource("/file", "File"); //collection of persons?
 
-//Projects plugin;
-$app->route->add("/projects{format}", "projects", "Project");
-$app->route->attachResource("/project", "Project"); //multiple projects types and status e.g project proposal
 
 //Protocols plugin
 $app->route->add("/protocols{format}", "protocols", "Protocol");
